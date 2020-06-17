db9693168d589cf2f88673269cea235f46e898b7
xingluaho <2621935866@qq.com>
2020-06-17 23:01:06 +0800

    构建项目、配置全局样式、路由、element-ui

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="login">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
+
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect:"/login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login/index")
+  }
+];
+
+export default new VueRouter({
+  routes
+});
